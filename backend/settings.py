81b0e4f3085f5437c4e61687ba2fd729b0301424
ZiggyEmmanuel <109115891+ZiggyEmmanuel@users.noreply.github.com>
2025-04-11 06:14:41 +0100

    Fix ALLOWED_HOSTS and other environment variable error

--- backend/settings.py
+++ backend/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get('DEBUG')
 
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
 # Application definition
 
