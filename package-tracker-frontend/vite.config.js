f5457d2cfdc0402dbd5b63ee5072223cbd6e3959
ZiggyEmmanuel <109115891+ZiggyEmmanuel@users.noreply.github.com>
2025-04-12 06:32:26 +0100

    SetUp vite config to use environment variables

--- package-tracker-frontend/vite.config.js
+++ package-tracker-frontend/vite.config.js
@@ -1,19 +1,23 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [tailwindcss(), react()],
-  server: {
-    port: 5173, // Make sure this matches the port in your Django CORS_ALLOWED_ORIGINS
-    open: true,
-    proxy: {
-      // If you want to proxy API requests during development
-      "/api": {
-        target: import.meta.env.VITE_API_URL, // Dynamically use the env var for Backend server URL
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current directory
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [tailwindcss(), react()],
+    server: {
+      port: 5173, // Ensure this matches the port in your Django CORS_ALLOWED_ORIGINS
+      open: true,
+      proxy: {
+        "/api": {
+          target: env.VITE_API_URL, // Now using the loaded env variables
+          changeOrigin: true,
+        },
       },
     },
-  },
+  };
 });
